8ce44bfa5347ee0256425a7f5f93b197012fbc0a
Anthony Mahramus <anthonym@anthonys-mbp.lan>
2018-04-27 23:18:19 -0400

    new changes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var apiRoutes = require("./routing/apiRoutes");
-var htmlRoutes = require("./routing/htmlRoutes");
+var apiRoutes = require("./app/routing/apiRoutes");
+var htmlRoutes = require("./app/routing/htmlRoutes");
 
 var app = express();
 
